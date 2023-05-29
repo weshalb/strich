729f9a2a9779582110ef8f7ebba5a133a2440bc3
weshalb <96797858+weshalb@users.noreply.github.com>
2023-05-30 01:10:29 +0200

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,28 @@
+// Function to update the HTML with the latest song details
+function updateSongContainer(song) {
+  const songContainer = document.getElementById('song-container');
+  songContainer.innerHTML = `
+    <h2>${song.name}</h2>
+    <p>By ${song.artists[0].name}</p>
+    <p>Album: ${song.album.name}</p>
+    <img src="${song.album.images[0].url}" alt="Album Cover" style="max-width: 200px;">
+  `;
+}
+
+// Function to fetch the latest song of a Spotify artist
+async function getLatestSong(artistId) {
+  const response = await fetch(`/latest-song?artistId=00UL27CiQhemd3BmIK8cDE`);
+  const song = await response.json();
+
+  if (song) {
+    updateSongContainer(song);
+  } else {
+    console.error('Error fetching latest song');
+  }
+}
+
+// Call the function with the artist ID when the page loads
+window.addEventListener('load', function() {
+  // Replace 'ARTIST_ID' with the actual ID of the desired Spotify artist
+  getLatestSong('00UL27CiQhemd3BmIK8cDE');
+});
