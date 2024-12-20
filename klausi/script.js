c6d636b06ef65abee6277c993861254907e93a7b
weshalb <96797858+weshalb@users.noreply.github.com>
2024-12-20 08:50:10 +0100

    Update script.js

--- klausi/script.js
+++ klausi/script.js
@@ -1,4 +1,4 @@
-import { PDFDocument } from "https://cdn.jsdelivr.net/npm/pdf-lib/dist/pdf-lib.min.js";
+import { PDFDocument } from "https://cdn.jsdelivr.net/npm/pdf-lib@1.17.1/dist/pdf-lib.min.js";
 import * as mammoth from "https://cdn.jsdelivr.net/npm/mammoth/mammoth.browser.min.js";
 
 const fileInput = document.getElementById("fileInput");
